7da5535ac74f32dbc68c02929b222ba77677063c
dingxin <zzuxind@163.com>
2019-08-25 15:59:34 +0800

    finally

--- main/best-charge.js
+++ main/best-charge.js
@@ -1,7 +1,7 @@
-
+var allPromotions=loadPromotions();
 var allItems=loadAllItems();
 function bestCharge(selectedItems) {
-return /*TODO*/;
+  return printReceipt(selectedItems);
 }
 /**
  * 整理订单
@@ -42,8 +42,108 @@ function getAllItemDetail(orderMap){
   let allItemDetailMap=new Map();
   orderMap.forEach((value,key) => {
     let itemDetail=getItemDetail(key);
+    itemDetail.count=value;
     allItemDetailMap.set(key,itemDetail);
   });
   return allItemDetailMap;
 }
 
+
+/**
+ * 计算订单总价
+ * map{id,{name:,price:,count}} => number
+ */
+function TotalPrice(allItemDetailMap){
+  let totalPrice=0;
+  allItemDetailMap.forEach((value,key) => {
+    let itemPrice=(value.price)*(value.count);
+    totalPrice+=itemPrice;
+  });
+  return totalPrice;
+}
+
+/**
+ * 选择满30减6元的优惠后的价格
+ * totalPrice => {discountPrice:number,saveMoney:}
+ */
+function discountIfEnoughThirty(totalPrice){
+  let discountPrice=totalPrice >= 30?totalPrice-6:totalPrice;
+  let discountObject={discountPrice:discountPrice,saveMoney:totalPrice-discountPrice};
+  return discountObject;
+}
+
+/**
+ * 指定菜品半价后优惠价格
+ * map{id,{name:,price:,count}} => {discountPrice:number,saveMoney:}
+ */
+function HalfPriceForGivenItems(allItemDetailMap,givenItems){
+  let totalPrice=TotalPrice(allItemDetailMap);
+  allItemDetailMap.forEach((value,key) => {
+     for (let index = 0; index < givenItems.length; index++) {
+       const element = givenItems[index];
+       if(key == element){
+         value.price=value.price/2;
+         break;
+       }
+     }
+  });
+  let discountedPrice=TotalPrice(allItemDetailMap);
+  let discountedObject={discountPrice:discountedPrice,saveMoney:totalPrice-discountedPrice};
+  return discountedObject;
+}
+
+/**
+ * 选择优惠方式
+ * map{id,{name:,price:,count}},givenItems => {promotion:,discountPrice:,saveMoney:}
+ */
+function choosePromotion(allItemDetailMap,allPromotions){
+  let promotionDetail={};
+  let tempObject;
+  let totalPrice=TotalPrice(allItemDetailMap);
+  let min=totalPrice;
+  allPromotions.forEach(element => {
+      if(element.type=="满30减6元"){
+        tempObject=discountIfEnoughThirty(totalPrice);
+        if(tempObject.discountPrice<min){
+          min=tempObject.discountPrice;
+          promotionDetail={promotion:element.type,discountPrice:tempObject.discountPrice,saveMoney:tempObject.saveMoney};
+        }
+      }if(element.type=="指定菜品半价"){
+        tempObject=HalfPriceForGivenItems(allItemDetailMap,element.items);
+        if(tempObject.discountPrice<min){
+          min=tempObject.discountPrice;
+          promotionDetail={promotion:element.type+"(黄焖鸡，凉皮)",discountPrice:tempObject.discountPrice,saveMoney:tempObject.saveMoney};
+        }
+        
+      }
+    });
+  return promotionDetail;
+}
+
+/**
+ * 打印小票
+ *order[] => string
+ */
+function printReceipt(order){
+  let head="============= 订餐明细 =============\n";
+  let middle="-----------------------------------\n";
+  let middleOrder="";
+  allItemsDetailMap=getAllItemDetail(countProducts(order));
+  let totalPrice=TotalPrice(allItemsDetailMap);
+  allItemsDetailMap.forEach((value) => {
+    let itemDetail=value.name+" x "+value.count+" = "+value.count*value.price+"元\n";
+    middleOrder+=itemDetail;
+  });
+  choosedPromotion=choosePromotion(allItemsDetailMap,allPromotions);
+  let footerDetail="使用优惠:\n"+choosedPromotion.promotion+",省"+choosedPromotion.saveMoney+"元\n";
+  let discountedTotalPrice="总计："+choosedPromotion.discountPrice+"元\n";
+  let end="===================================";
+  if(choosedPromotion == null){
+    return head+middleOrder+middle+footerDetail+middle+discountedTotalPrice+end;
+  }else{
+    return head+middleOrder+middle+"总计:"+totalPrice+"\n"+end;
+  }
+  
+  
+  
+}
--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -1,5 +1,11 @@
 
 var order=["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+var allPromotions=[{
+  type: '满30减6元'
+}, {
+  type: '指定菜品半价',
+  items: ['ITEM0001', 'ITEM0022']
+}];
 
 describe('Take out food', function () {
 
@@ -11,29 +17,72 @@ expect(countProduct.get("ITEM0001")).toBe(1);
 
 it("get menu detail by id",()=>{
   let itemDetail=getItemDetail("ITEM0001");
-  expect(itemDetail.name).toEqual("黄焖鸡");
-  });
+  expect(itemDetail.name).toEqual("黄焖鸡");  
+});
 
-  it("get all menu detail by input orderMap",()=>{
-    let allMenueMap=getAllItemDetail(countProducts(order));
-    console.log(allMenueMap);
-    expect(allMenueMap.get("ITEM0001").name).toEqual("黄焖鸡");
+it("get all menu detail by input orderMap",()=>{
+  let allMenueMap=getAllItemDetail(countProducts(order));
+  console.log(allMenueMap);
+  expect(allMenueMap.get("ITEM0001").count).toBe(1);
+});
+
+it("should return total price",()=>{
+  let allMenueMap=getAllItemDetail(countProducts(order));
+  let totalPrice=TotalPrice(allMenueMap);
+  expect(totalPrice).toBe(38);
+})
+
+it("should substract ￥6 if enough ￥30",()=>{
+  let allMenueMap=getAllItemDetail(countProducts(order));
+  let totalPrice=TotalPrice(allMenueMap);
+  let discountObject=discountIfEnoughThirty(totalPrice);
+  expect(discountObject.discountPrice).toBe(32);
+})
+
+it("should half price for given items",()=>{
+  let allMenueMap=getAllItemDetail(countProducts(order));
+  let discountedObject=HalfPriceForGivenItems(allMenueMap,['ITEM0001', 'ITEM0022']);
+  expect(discountedObject.discountPrice).toBe(25);
+})
+
+it("should choose second promotion",()=>{
+  let allMenueMap=getAllItemDetail(countProducts(order));
+  let promotionDetail=choosePromotion(allMenueMap,allPromotions);
+  expect(promotionDetail.promotion).toEqual("指定菜品半价(黄焖鸡，凉皮)");
+})
+
+it('should generate best charge when best is 指定菜品半价', function() {
+      let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+      let summary = bestCharge(inputs).trim();
+      console.log(summary);
     });
 
+    it('should generate best charge when best is 满30减6元', function() {
+          let inputs = ["ITEM0013 x 4", "ITEM0022 x 1"];
+          let summary = bestCharge(inputs).trim();
+          console.log(summary);
+        });
+      
+        it('should generate best charge when no promotion can be used', function() {
+          let inputs = ["ITEM0013 x 4"];
+          let summary = bestCharge(inputs).trim();
+          console.log(summary);
+        });
 // it('should generate best charge when best is 指定菜品半价', function() {
 //     let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
 //     let summary = bestCharge(inputs).trim();
 //     let expected = `
-// ============= 订餐明细 =============
-// 黄焖鸡 x 1 = 18元
-// 肉夹馍 x 2 = 12元
-// 凉皮 x 1 = 8元
-// -----------------------------------
-// 使用优惠:
-// 指定菜品半价(黄焖鸡，凉皮)，省13元
-// -----------------------------------
-// 总计：25元
-// ===================================`.trim()
+//     ============= 订餐明细 =============
+//     黄焖鸡 x 1 = 18元
+//     肉夹馍 x 2 = 12元
+//     凉皮 x 1 = 8元
+//     -----------------------------------
+//     使用优惠:
+//     指定菜品半价(黄焖鸡，凉皮),省13元
+//     -----------------------------------
+//     总计：25元
+//     ===================================`.trim()
+
 //     expect(summary).toEqual(expected)
 //   });
 
